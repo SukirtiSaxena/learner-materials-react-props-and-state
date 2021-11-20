e72e53da78ba84a0e6441980f421b2d62f69d77d
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-11-20 22:06:54 +0000

    Destructuring and assign data to 'props'

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -1,11 +1,12 @@
 function CatCard(props) {
+    const {name,alt,species,favFoods,birthYear,photo} = props
     return (
         <div className="card">
-            <h3 className="card__text card__header"> {props.name} </h3>
-            <img className="card__image" src={props.photo} alt={props.alt}></img>
-            <p className="card__text">Species: {props.species}</p>
-            <p className="card__text">Favourite Food(s): {props.favFoods}</p>
-            <p className="card__text">Birth Year: {props.birthYear}</p>
+            <h3 className="card__text card__header"> {name} </h3>
+            <img className="card__image" src={photo} alt={alt}></img>
+            <p className="card__text">Species: {species}</p>
+            <p className="card__text">Favourite Food(s): {favFoods}</p>
+            <p className="card__text">Birth Year: {birthYear}</p>
         </div>
     )
 }
